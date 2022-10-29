c108479ac4507ffc280101df858c2983e3b80e4d
Syeta <ganglisenokilya@gmail.com>
2022-10-29 13:21:32 +0300

    mwindow2

--- src/js/components/modalWindow/mWindow.js
+++ src/js/components/modalWindow/mWindow.js
@@ -18,6 +18,13 @@ let mwindowHtml = generalmwindow.map((a) => {
                     <p style="color: rgb(62, 62, 255); font-weight: 700;">
                         ${a.text4}  
                     </p>
+                <div style="padding: 30px 0 0 0;">
+                    <a class="a1 text-light" href="">${a.btn1}</a>
+                    <a class="a2 text-light" href="">${a.btn2}</a>
+                    <a class="a3 text-light" href="">${a.btn3}</a>
+                    <a class="a4 text-light" href="">${a.btn4}</a>
+                    <a class="a5 text-light" href="">${a.btn5}</a>
+                </div>
                 </div>
                 <div class="col-md-4">
 
@@ -25,6 +32,110 @@ let mwindowHtml = generalmwindow.map((a) => {
             </div>
         </div>
     </div>
+
+
+    <style type="text/css">
+
+
+
+    .a1{
+        color:#fff; 
+        background:#F08080;
+        border-radius: 50%;
+        padding: 10px 17px 10px 17px;
+        text-decoration: none;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 	
+        text-transform: uppercase;
+        border: solid 2px #CD5C5C;
+        font-weight: 600;
+    }
+    .a1:hover {
+        opacity: 0.7;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 
+    }   
+    .a2{
+        color:#fff; 
+        background:LightSalmon;
+        border-radius: 50%;
+        padding: 10px 17px 10px 17px;
+        text-decoration: none;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 	
+        text-transform: uppercase;
+        border: solid 2px #FF7F50;
+        font-weight: 600;
+    }
+    .a2:hover {
+        opacity: 0.7;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 
+    }   
+    .a3{
+        color:#fff; 
+        background:#ffd078;
+        border-radius: 50%;
+        padding: 10px 17px 10px 17px;
+        text-decoration: none;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 	
+        text-transform: uppercase;
+        border: solid 2px orange;
+        font-weight: 600;
+    }
+    .a3:hover {
+        opacity: 0.7;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 
+    }   
+    .a4{
+        color:#fff; 
+        background: rgb(160, 218, 46);
+        border-radius: 50%;
+        padding: 10px 16px 10px 16px;
+        text-decoration: none;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 	
+        text-transform: uppercase;
+        border: solid 2px YellowGreen;
+        font-weight: 600;
+    }
+    .a4:hover {
+        opacity: 0.7;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 
+    }   
+    .a5{
+        color:#fff; 
+        background:rgba(140, 184, 53, 0.873);
+        border-radius: 50%;
+        padding: 10px 16px 10px 16px;
+        text-decoration: none;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 	
+        text-transform: uppercase;
+        border: solid 2px rgb(132, 173, 61);
+        font-weight: 600;
+    }
+    .a5:hover {
+        opacity: 0.7;
+        tansition: all .3s linear;
+        -webkit-transition: all .3s linear;
+        -moz-transition: all .3s linear; 
+    }   
+   </style>
+
+
     `
 }).join(" ");
 
