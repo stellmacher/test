abdec8468d112ea3c9d9e258410d2d5725cb25c7
Plain <kovalchuk.baksmail.ru@gmail.com>
2022-11-19 14:09:29 +0300

    asdf

--- src/js/components/top_first/top_first.js
+++ src/js/components/top_first/top_first.js
@@ -14,9 +14,9 @@ let top_firstHtml = generaltop_first.map((a) => {
             <h5 class="feedback_text" style="font-weight: 400; font-size: 15px; padding: 20px 0;">${a.text2}</h5>
         </div>
 
-        <div class="questions_Coddy_text" >
-            <form action="text"></form>
-            <p><textarea name="comment" cols="40" rows="3" style: background-color: transparent;>${a.text3}</textarea></p>
+        <div style=:border-radius: 15px; color: transparent; class="questions_Coddy_text">
+            <form style="background-color: transparent;" action="text"></form>
+                <p><textarea placeholder="${a.text3}" name="comment" cols="40" rows="3"></textarea></p>
             </form>
         </div>
 
@@ -32,7 +32,7 @@ let top_firstHtml = generaltop_first.map((a) => {
             <p class="privacy_text" style="display: flex; font-size: 10px;">${a.text6} <a href="#" style="color: #00008B;">${a.text7}</a> ${a.text8}<a href="#" style="color: #00008B;">${a.text9}</a></p>
         </div>
 
-        <button style="width: 150px; background-color: #00FF00; margin: 20px 15px; border-radius: 15px; border-style: none; border-width: 0; border-color: transparent;"><a href="#" style="font-size: 15px; display:flex; color: whitesmoke;">${a.text10}</a></button>
+        <button style=" background-color: #00db6a; margin: 20px 15px; box-shadow: inset 0px -5px 0px 0px rgba(0,0,0,0.5); border-radius: 25px; border-style: none; border-width: 0; border-color: transparent; width: 170px; height: 50px;"><a href="#" style="font-size: 15px; display:flex; color: whitesmoke; padding: 0 12px;">${a.text10}</a></button>
     </div>
 </div>
     `
